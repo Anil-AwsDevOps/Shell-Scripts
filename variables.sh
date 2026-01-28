6b655cc7dd693bc83cc45ba172d2e72a0c14f45b
Anil <anilbeeraka99@gmail.com>
2026-01-28 15:41:03 +0530

    actions

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-PERSON1=Anil
-PERSON2=Bob
+PERSON1=Kohli
+PERSON2=Rohit
 echo "$PERSON1 : Hi $PERSON2, How are you ?"
-echo "$PERSON2: Hi $PERSON1, I am fine $PERSON1"
+echo "$PERSON2: Hi $PERSON1, I am fine "
