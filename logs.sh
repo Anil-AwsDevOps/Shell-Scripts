766d9ef8da17b1ade4d5c9d2cdfe98bec44f8c2b
Anil <anilbeeraka99@gmail.com>
2026-01-29 15:13:15 +0530

    actions

--- logs.sh
+++ logs.sh
@@ -6,14 +6,14 @@ LOGS_FOLDER="/var/log/shellscript"
 LOGS_FILE="/var/log/shellscript/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "run this as root user"
+    echo "run this as root user" | tee -a $LOGS_FILE
     exit 1
 fi
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2  is failure"
+        echo "$2  is failure" | tee -a $LOGS_FILE
     else
-        echo "$2  is success"
+        echo "$2  is success" | tee -a $LOGS_FILE
     fi
 }
 
